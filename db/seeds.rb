136d563a3bd9aeb44faa5de6f243c4c9a54b80be
Amanda Porto <amandaporto@msn.com>
2015-11-02 12:42:19 -0500

    Adds seeds to tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Activity.create([name:'Volleyball'])
+Activity.create([name:'Soccer'])
+Activity.create([name:'Biking'])
+Activity.create([name:'Tenis'])
+Activity.create([name:'Running'])
+Activity.create([name:'Baseball'])
+Activity.create([name:'Football'])
+Activity.create([name:'Basketball'])
+
+
+User.create([name:"Gavin Startk", address:"St Pete, FL"])
+User.create([name:"Foo Bar", address:"Tampa, FL"])
+User.create([name:"Jason Perry", address:"St Pete, FL"])
