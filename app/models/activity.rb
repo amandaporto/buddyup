b0e0f6b49d2c7fa3f2f13a77bc38ff6c171808bd
Amanda Porto <amandaporto@msn.com>
2015-11-03 12:52:25 -0500

    Adds enum to sport and skill level

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,6 @@
 class Activity < ActiveRecord::Base
+
+  enum skill_level: { Beg: 0, Int: 1, Adv: 2, Pro: 3 }
+
+  enum sport: { Volleyball: 0, Soccer: 1, Biking: 2, Basketball: 3, Football: 4, Baseball: 5, Tennis: 6 }
 end
