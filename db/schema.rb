589554f6c7d92fd858ce2702ea1a2726f23f5f04
Amanda Porto <amandaporto@msn.com>
2015-11-04 11:41:32 -0500

    Adds latitude and longitude columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103150955) do
+ActiveRecord::Schema.define(version: 20151104163836) do
 
   create_table "activities", force: :cascade do |t|
     t.datetime "created_at",              null: false
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20151103150955) do
     t.string   "address"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
