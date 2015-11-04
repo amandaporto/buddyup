5e485a2d2f3f52fcb3ba81ae4b9ebbdf83ce9a59
Amanda Porto <amandaporto@msn.com>
2015-11-04 09:55:56 -0500

    Removes unecessary images and styling changes on skill level and sport images

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ActiveRecord::Base
 
-  enum skill_level: { Beg: 0, Int: 1, Adv: 2, Pro: 3 }
+  enum skill_level: { Beginner: 0, Intermediate: 1, Advanced: 2, Professional: 3 }
 
   enum sport: { Volleyball: 0, Soccer: 1, Biking: 2, Basketball: 3, Football: 4, Baseball: 5, Tennis: 6 }
 end
