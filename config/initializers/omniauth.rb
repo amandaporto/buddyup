e09ba1bc73be7e4ebea68302ad50c348e54d967f
Amanda Porto <amandaporto@msn.com>
2015-11-04 13:10:23 -0500

    Profile pictures coming from omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-           scope: 'email, public_profile', info_fields:'email, name', :image_size => 'square'
+           scope: 'email, public_profile', info_fields:'email, name', image_size: 'large'
 
   provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"],
-           scope: 'name', :image_size => 'original'
+           scope: 'name', image_size: 'original'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-foo = User.create(name:"Foo Bar", address:"Tampa, FL")
-foo.activities.create([sport: "Soccer", skill_level: "Beg"])
+foo = User.create(name:"Foo Bar", zip:"Tampa, FL")
+foo.activities.create([sport: "Soccer", skill_level: "Beginner"])
 
-jason = User.create(name:"Jason Perry", address:"St Pete, FL")
-jason.activities.create([sport: "Baseball", skill_level: "Int"])
+jason = User.create(name:"Jason Perry", zip:"St Pete, FL")
+jason.activities.create([sport: "Baseball", skill_level: "Professional"])
 
-gavin = User.create(name:"Gavin Startk", address:"St Pete, FL")
-gavin.activities.create([sport: "Biking", skill_level: "Pro"])
+gavin = User.create(name:"Gavin Startk", zip:"St Pete, FL")
+gavin.activities.create([sport: "Biking", skill_level: "Advanced"])
